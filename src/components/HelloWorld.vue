fe5c4590a64c94f719fabd185d97c4d2e100353f
Hans-lee-2006 <hanslee2006@gmail.com>
2020-02-24 15:13:28 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,9 @@
 <template>
   <div class="hello">
+  <img src="https://lh3.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP=s360-rw">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <a href="https://www.minecraft.net/zh-hant/" target = "_blank">官網</a>
+   <p>《當個創世神》（英語：Minecraft，中國大陸譯作「我的世界」）是一款沙盒遊戲，最初由瑞典遊戲設計師馬庫斯·阿列克謝·泊松（別名Notch）單獨開發，隨後由2009年成立的瑞典公司Mojang開發並發行。玩家可以在一個隨機生成的3D世界內，以帶材質貼圖的立方體為基礎進行遊戲。遊戲中的其他特色包括探索世界、採集資源、合成物品及生存冒險等。遊戲有多種模式，生存模式中玩家必須維持生命並採集資源來打造自己的世界；創造模式中玩家擁有無限的資源並可飛行，大多數玩家會使用此模式來建造大型建築；冒險模式中玩家可在其他玩家客製化的地圖中遊玩。遊戲的Java版以其強大的第三方拓展模組而知名，它們可為遊戲添加各種新的物件、角色和功能。</p>
   </div>
 </template>
 
@@ -96,6 +20,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+p {
+max-width: 600px;
+}
 h1, h2 {
   font-weight: normal;
 }
