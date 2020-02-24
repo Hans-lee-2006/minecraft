f450b30b383d5861a92d836d28c4ca0cbf9066fb
Bestian Tang <bestian@gmail.com>
2020-02-24 15:24:31 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,13 +36,8 @@ p {
 h1, h2 {
   font-weight: normal;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+ol {
+  list-style-type:decimal;
 }
 a {
   color: #42b983;
