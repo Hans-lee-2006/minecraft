69a2da88ebb54c4c6c5da62f8844dfaccd4e2991
Hans-lee-2006 <hanslee2006@gmail.com>
2020-03-08 18:16:59 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,7 @@
           <li><a href="https://www.minecraft.net/zh-hant/" target = "_blank">官網</a></li>
           <li><a href="https://www.curseforge.com/minecraft/modpacks" target = "_blank">全球最大的模組(mod)網站</a></li>
           <li><a href="https://mcfuns.com.tw/category/%E9%81%8A%E6%88%B2%E6%9D%90%E8%B3%AA/%E5%85%89%E5%BD%B1%E5%8C%85/" target = "_blank">繁中mod網站</a></li>
+          <li><a href="https://howtobullshit.me/" target = "_blank">廢文產生器</a></li>
         </ol>
       </div>
     </div>
@@ -21,7 +22,7 @@
       </div>
     </div>
     <div class="container">
-      <p>這裡是我的個人網站我會放上我自己的地圖，跟我正在玩的模組，跟我發現的有趣的網站。當然主要都還是跟minecraft有關，畢竟這是我唯一有玩的遊戲啊!</p>
+      <p>這裡是我的個人網站我會放上我自己的地圖，跟我正在玩的模組，跟我發現的有趣的網站。當然主要都還是跟minecraft有關，但還是會有一些其他的，畢竟這是我唯一有玩的遊戲啊!</p>
     </div>
   </div>
 </template>
