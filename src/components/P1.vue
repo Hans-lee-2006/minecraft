cf07281700faffcd40bad555916fa03fcf668afe
Bestian Tang 小巴 <bestian@gmail.com>
2020-04-06 15:04:00 +0800

    Update P1.vue

--- src/components/P1.vue
+++ src/components/P1.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="hello">
-  <img class="mc" src="https://lh3.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP=s360-rw">
-    <h1>{{ msg }}</h1>
-    <div class="container">
-      <div class="block">
-        <h2>請看說明</h2>
-        <ol>
-          <li v-for="l in links" v-bind:key="l.name">
-            <a id ="big" :href="l.url" target="_blank">
-              <span :class="l.class">{{ l.name }}</span>
-            </a>
-          </li>
-        </ol>
-      </div>
-    </div>
-    <div class="container">
-      <p>這裡是我的個人網站我會放上我自己的地圖，跟我正在玩的模組，跟我發現的有趣的網站。當然主要都還是跟minecraft有關，但還是會有一些其他的，畢竟這是我唯一有玩的遊戲啊!</p>
-    </div>
   </div>
 </template>
 
@@ -25,12 +8,6 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to  minecraft',
-      links: [
-        {name: '寫給麻瓜', url: 'https://hackmd.io/HuVYJXouSi6uZd8oNXmJQA?view', class: 'blue'},
-        {name: '寫給初學者', url: 'https://hackmd.io/5fJF_mU8S0e915U6h7dG2w?view', class: 'green'},
-        {name: '寫給老手', url: 'https://hackmd.io/A7Wgqb4IQh27zXzZ7BbTFQ?view', class: 'red'}
-      ]
     }
   }
 }
