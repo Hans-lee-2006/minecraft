fb2f461c09e6ad507937cd5c0912b0ae743d0685
Bestian Tang 小巴 <bestian@gmail.com>
2020-04-06 15:23:08 +0800

    create p2, p3

---
+++ src/components/P2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <iframe src="https://hackmd.io/5fJF_mU8S0e915U6h7dG2w?view"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.hello {
+  margin-top: 50px;
+}
+
+iframe {
+  width: 100%;
+  height: 100vh;
+  margin-bottom: -50px; 
+}
+
+</style>
