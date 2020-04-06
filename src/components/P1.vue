d94cbe910c7c49933fb51f29ed74bea311472295
Bestian Tang 小巴 <bestian@gmail.com>
2020-04-06 15:05:39 +0800

    Update P1.vue

--- src/components/P1.vue
+++ src/components/P1.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <iframe src="https://hackmd.io/HuVYJXouSi6uZd8oNXmJQA?view"></iframe>
   </div>
 </template>
 
@@ -16,46 +17,10 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-#big {
-  font-size: 1.5em;
-  text-decoration: none;
+
+iframe {
+  width: 100%;
+  height: 80vh;
 }
 
-.blue{
-  color: blue;
-}
-.green{
-}
-.red{
-  color: red;
-  /* text-shadow: 2px 2px 0px red;*/
-}
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.block {
-  width: 250px;
-}
-p {
-  max-width: 600px;
-}
-h1, h2 {
-  font-weight: normal;
-}
-ol {
-  list-style-type:decimal;
-}
-
-li {
-  font-size: 22px;
-}
-
-a {
-  color: #42b983;
-}
-.mc {
-  width: 20vmin;
-}
 </style>
