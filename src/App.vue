697eb57b6523eb940c0cff9c8f7b11ccb89af199
Hans-lee-2006 <hanslee2006@gmail.com>
2020-04-11 09:59:48 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
  <template>
   <div id="app">
     <nav class="ui menu">
-      <router-link to = "/">home</router-link>
-      <router-link to = "/myworld">我的作品</router-link>
-      <router-link to = "/p1">寫給麻瓜</router-link>
-      <router-link to = "/p2">寫給初學者</router-link>
-      <router-link to = "/p3">寫給老手</router-link>
+      <router-link class = "fat-only" to = "/">home</router-link>
+      <router-link class = "fat-only" to = "/myworld">我的作品</router-link>
+      <router-link class = "fat-only" to = "/p1">寫給麻瓜</router-link>
+      <router-link class = "fat-only" to = "/p2">寫給初學者</router-link>
+      <router-link class = "fat-only" to = "/p3">寫給老手</router-link>
+      <router-link class = "thin-only" to = "/">home</router-link>
+      <router-link class = "thin-only" to = "/myworld">作品</router-link>
+      <router-link class = "thin-only" to = "/p1">麻瓜</router-link>
+      <router-link class = "thin-only" to = "/p2">初學</router-link>
+      <router-link class = "thin-only" to = "/p3">老手</router-link>
     </nav>
     <router-view/>
   </div>
