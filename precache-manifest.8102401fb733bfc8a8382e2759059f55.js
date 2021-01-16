55d796420aa2975e97116c519bdd6fcb331df4e5
Hans-lee-2006 <hanslee2006@gmail.com>
2021-01-16 09:35:29 +0800

    deploy

---
+++ precache-manifest.8102401fb733bfc8a8382e2759059f55.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fde0763cbf9b02c524",
+    "url": "/minecraft/css/app.449f4a3b.css"
+  },
+  {
+    "revision": "80fde0763cbf9b02c524",
+    "url": "/minecraft/css/app.449f4a3b.css.map"
+  },
+  {
+    "revision": "84109c52dbb5d2ff4d03aa6427607ec6",
+    "url": "/minecraft/favicon.ico"
+  },
+  {
+    "revision": "a2f7ae874350d646fba3bc0df2dc220b",
+    "url": "/minecraft/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "843734a7b98dfde7a4ad48d4616fa6f6",
+    "url": "/minecraft/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a2f7ae874350d646fba3bc0df2dc220b",
+    "url": "/minecraft/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "843734a7b98dfde7a4ad48d4616fa6f6",
+    "url": "/minecraft/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "bbef3a40a012df0760cc919e00df5507",
+    "url": "/minecraft/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "53374a41ac26e5262466c64e74b973b7",
+    "url": "/minecraft/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "07c0edf332ec1ba9ba3146573d0e564d",
+    "url": "/minecraft/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "cf852ff0132d4bbef2b5a32930e15a7b",
+    "url": "/minecraft/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "f68563b5f8b13b7ee6b32d37c966e534",
+    "url": "/minecraft/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "07c0edf332ec1ba9ba3146573d0e564d",
+    "url": "/minecraft/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "c32c77197143afa2a7e39196c4fe3ac4",
+    "url": "/minecraft/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "8c472aa9f32945e49fd74be16fc62b25",
+    "url": "/minecraft/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "84109c52dbb5d2ff4d03aa6427607ec6",
+    "url": "/minecraft/img/icons/favicon.ico"
+  },
+  {
+    "revision": "ca9b72d123a80d2915c1292b975dfec1",
+    "url": "/minecraft/img/icons/manifest.json"
+  },
+  {
+    "revision": "d108289201b8ac82b7861df80fde3dbf",
+    "url": "/minecraft/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "bf8a0e5b58fbd0af135829e417aecde6",
+    "url": "/minecraft/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "906d601c6249dbf747e01ea26dc92441",
+    "url": "/minecraft/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "10de37723f74a98689ab78a54129b7a5",
+    "url": "/minecraft/index.html"
+  },
+  {
+    "revision": "80fde0763cbf9b02c524",
+    "url": "/minecraft/js/app.6f40aadf.js"
+  },
+  {
+    "revision": "80fde0763cbf9b02c524",
+    "url": "/minecraft/js/app.6f40aadf.js.map"
+  },
+  {
+    "revision": "6a0e4df8573f99e25891",
+    "url": "/minecraft/js/chunk-vendors.bdf45aa7.js"
+  },
+  {
+    "revision": "6a0e4df8573f99e25891",
+    "url": "/minecraft/js/chunk-vendors.bdf45aa7.js.map"
+  },
+  {
+    "revision": "13f43d0b8f915c4b968a622a4059773b",
+    "url": "/minecraft/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/minecraft/robots.txt"
+  },
+  {
+    "revision": "356698dc921bce5f27e98a1ff95c48ad",
+    "url": "/minecraft/static/bg-img.jpeg"
+  },
+  {
+    "revision": "d470f7227e1368c64d2a07792b692a4c",
+    "url": "/minecraft/static/favicon.ico"
+  },
+  {
+    "revision": "5a41fb1fc6247a27647fc3801d65c470",
+    "url": "/minecraft/static/logo.jpg"
+  },
+  {
+    "revision": "5277931d7fe83c200ea72b08d2a6599f",
+    "url": "/minecraft/static/multicolored-abstract-painting-1509534.jpg"
+  },
+  {
+    "revision": "79f1c366cf5d0a868c0515e1986d90b0",
+    "url": "/minecraft/static/ray 空島.zip"
+  },
+  {
+    "revision": "a2ef0471a17acf097a71a823dc25edf1",
+    "url": "/minecraft/static/rustic-surface-texture-wood-1303092.jpg"
+  },
+  {
+    "revision": "68237f7c315cfa053913e17babf6e7c5",
+    "url": "/minecraft/static/white-painted-wall-1939485.jpg"
+  },
+  {
+    "revision": "8ffdbc25abb5f73d52859ceb8318ff08",
+    "url": "/minecraft/static/生存.zip"
+  }
+]);
